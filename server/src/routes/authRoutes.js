1fef08a113fb4b1c68d70b5efce54d8eba9e69f3
vishxlkr <vishxlkr@gmail.com>
2026-04-24 12:01:18 +0530

    update

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
 const { register, login, getMe } = require("../controllers/authController");
 const requireAuth = require("../middlewares/authMiddleware");
+const validateRequest = require("../middlewares/validateRequest");
 
-router.post("/register", register);
-router.post("/login", login);
+router.post(
+   "/register",
+   [
+      body("email").isEmail().withMessage("Please include a valid email"),
+      body("password")
+         .isLength({ min: 6 })
+         .withMessage("Password must be at least 6 characters long"),
+   ],
+   validateRequest,
+   register,
+);
+
+router.post(
+   "/login",
+   [
+      body("email").isEmail().withMessage("Please include a valid email"),
+      body("password").exists().withMessage("Password is required"),
+   ],
+   validateRequest,
+   login,
+);
+
 router.get("/me", requireAuth, getMe);
 
 module.exports = router;
--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,18 +1,32 @@
-const express = require("express");
+const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
-const {
-   getTasks,
-   createTask,
-   updateTask,
-   deleteTask,
-} = require("../controllers/taskController");
-const requireAuth = require("../middlewares/authMiddleware");
+const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const requireAuth = require('../middlewares/authMiddleware');
+const validateRequest = require('../middlewares/validateRequest');
 
 router.use(requireAuth); // Protect all task routes
 
-router.get("/", getTasks);
-router.post("/", createTask);
-router.put("/:id", updateTask);
-router.delete("/:id", deleteTask);
+router.get('/', getTasks);
+
+router.post(
+  '/',
+  [
+    body('title').notEmpty().withMessage('Title is required')
+  ],
+  validateRequest,
+  createTask
+);
+
+router.put(
+  '/:id',
+  [
+    body('status').optional().isIn(['pending', 'completed']).withMessage('Status must be pending or completed')
+  ],
+  validateRequest,
+  updateTask
+);
+
+router.delete('/:id', deleteTask);
 
 module.exports = router;
